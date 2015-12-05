ecbfa60ef9329158c3f683993f8b0b7ebf19a214
icbat <cobb.evan@gmail.com>
2015-12-05 17:14:22 -0600

    the rest of day 2
    oops 1 commit.

--- python/day2/wrapping_paper_dimensions.py
+++ python/day2/wrapping_paper_dimensions.py
@@ -1,4 +1,63 @@
+from functools import reduce
+
 def breakup(line):
-    dimensions = [int(number) for number in line.split("X")]
-    dimensions.sort()
+    dimensions = [int(number) for number in line.split("x")]
+    print (line + " broke in to " + str(dimensions))
     return dimensions
+
+def double(x):
+    return 2*x
+
+def calculatesides(dimensions):
+    print ("Given dimensions " + str(dimensions))
+    areas = buildSortedAreas(dimensions)
+    smallest_side = areas[0]
+
+    cubic_area = sum(map(double, areas))
+    return cubic_area + smallest_side
+
+def buildSortedAreas(dimensions):
+    print ("Building Areas from " + str(dimensions))
+    length = dimensions[0]
+    width = dimensions[1]
+    height = dimensions[2]
+    areas = [length * width, length * height, width * height]
+    return sort(areas)
+
+def calculate_wrapping_paper(content):
+    content = cleanup(content)
+    dimensions = map(breakup, content)
+    paper_per_present = map(calculatesides, dimensions)
+    return sum(paper_per_present)
+
+def cleanup(content):
+    def trim(string):
+        return string.strip()
+    return map(trim, content)
+
+def sort(list):
+    list.sort()
+    return list
+
+def calculate_ribbon(content):
+    content = cleanup(content)
+    dimensions = map(breakup, content)
+    dimensions = list(map(sort, dimensions))
+    
+    all_around_presents = sum(list(map(ribbon_to_wrap, dimensions)))
+    print ("all_around_presents")
+    print (all_around_presents)
+    all_bows = sum( map(ribbon_for_bow, dimensions))
+
+    print (all_bows)
+    return all_around_presents + all_bows
+
+def ribbon_to_wrap(dimensions):
+    smallest_sides_dimensions = dimensions[:2]
+    print ("Found smallest side of " + str(smallest_sides_dimensions))
+    to_wrap = reduce ((lambda x, y: x+y), map( (lambda x: x+x) , smallest_sides_dimensions))
+    print ("ribbon to go around the sides: " + str(to_wrap))
+    return to_wrap
+
+def ribbon_for_bow(dimensions):
+    return reduce( (lambda x, y: x * y), dimensions )
--- python/day2/wrapping_paper_dimensions_test.py
+++ python/day2/wrapping_paper_dimensions_test.py
@@ -1,17 +1,61 @@
 import wrapping_paper_dimensions
 
 def test_breakup():
-    dimensions = wrapping_paper_dimensions.breakup("2X3X4")
+    dimensions = wrapping_paper_dimensions.breakup("2x3x4")
 
     assert len(dimensions) == 3
     assert dimensions[0] == 2
     assert dimensions[1] == 3
     assert dimensions[2] == 4
 
-def test_breakup_sorts():
-    dimensions = wrapping_paper_dimensions.breakup("5X4X3")
+def test_breakup_otherinput():
+    dimensions = wrapping_paper_dimensions.breakup("5x4x3")
 
     assert len(dimensions) == 3
-    assert dimensions[0] == 3
+    assert dimensions[0] == 5
     assert dimensions[1] == 4
-    assert dimensions[2] == 5
+    assert dimensions[2] == 3
+
+def test_calculatesides():
+    actual = wrapping_paper_dimensions.calculatesides([2, 3, 4])
+    print(str(actual))
+    assert actual == 58
+
+def test_calculatesides_otherinput():
+    actual = wrapping_paper_dimensions.calculatesides([1, 1, 10])
+    print(str(actual))
+    assert actual == 43
+
+def test_calculate_wrapping_test_data():
+    assert 58 == wrapping_paper_dimensions.calculate_wrapping_paper(["2x3x4"])
+    assert 43 == wrapping_paper_dimensions.calculate_wrapping_paper(["1x1x10"])
+
+def test_wrapping_readfile():
+    content = []
+    with open("input.secret") as f:
+        content = f.readlines()
+
+    paper = wrapping_paper_dimensions.calculate_wrapping_paper(content)
+    assert 1598415 == paper
+
+def test_ribbon():
+    assert 34 == wrapping_paper_dimensions.calculate_ribbon(["2x3x4"])
+    assert 14 == wrapping_paper_dimensions.calculate_ribbon(["1x1x10"])
+    assert 48 == wrapping_paper_dimensions.calculate_ribbon(["2x3x4", "1x1x10"])
+
+def test_ribbon_to_wrap():
+    assert 10 == wrapping_paper_dimensions.ribbon_to_wrap([2, 3, 4])
+    assert 4 == wrapping_paper_dimensions.ribbon_to_wrap([1, 1, 10])
+
+def test_ribbon_for_bows():
+    assert 24 == wrapping_paper_dimensions.ribbon_for_bow([2, 3, 4])
+    assert 10 == wrapping_paper_dimensions.ribbon_for_bow([1, 1, 10])
+
+def test_ribbon_readfile():
+    content = []
+    with open("input.secret") as f:
+        content = f.readlines()
+
+    ribbon = wrapping_paper_dimensions.calculate_ribbon(content)
+    print (ribbon)
+    assert 3812909 == ribbon
