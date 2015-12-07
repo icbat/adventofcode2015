b9def24eba28ef3aea63a7b5aa0de267c1b4d19e
icbat <cobb.evan@gmail.com>
2015-12-06 21:48:43 -0600

    day5 - refactoring after tests pass for evil strings

--- python/day5/naught_nice.py
+++ python/day5/naught_nice.py
@@ -1,4 +1,7 @@
 def niceness(string):
+    return contains_evil(string)
+
+def contains_evil(string):
     evil_strings = ["ab", "cd", "pq", "xy"]
     for evil in evil_strings:
         if evil in string:
